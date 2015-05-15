b5ff5be5a6d698e372fcb2089b1ac105339c7ef3
Seaborn Lee <seabornlee.cn@gmail.com>
2015-05-15 16:22:19 +0800

    Refactor steps

--- steps/login.py
+++ steps/login.py
@@ -3,21 +3,17 @@
 from nose.tools import *
 from behave import *
 
-@given(u'我输入加数{number}')
-def step_impl(context, number):
-    context.driver.find_element_by_name('TextField1').send_keys(number)
+@given(u'我在{text_field_id}输入{text}')
+def step_impl(context, text_field_id, text):
+    text_field = context.driver.find_element_by_name(text_field_id)
+    text_field.send_keys(text)
 
-@given(u'我输入被加数{number}')
-def step_impl(context, number):
-    num2 = context.driver.find_element_by_name('TextField2')
-    num2.send_keys(number)
+@when(u'我点击{text_on_button}按钮')
+def step_impl(context, text_on_button):
+    context.driver.find_element_by_accessibility_id(text_on_button).click()
 
-@when(u'我点击"计算"按钮')
-def step_impl(context):
-    context.driver.find_element_by_accessibility_id('ComputeSumButton').click()
-
-@then(u'我应该看到结果是{number}')
-def step_impl(context, number):
-    sum = context.driver.find_element_by_name('Answer').text
-    eq_(number, sum)
+@then(u'我应该看到{label_id}是{expected_text}')
+def step_impl(context, label_id, expected_text):
+    text = context.driver.find_element_by_accessibility_id(label_id).text
+    eq_(expected_text, text)
 
